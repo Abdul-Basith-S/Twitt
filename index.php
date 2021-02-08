a84deb869710cf5bd5ff2d6da3786b6554d6bdd7
Abdul Basith <arbasith123@gmail.com>
2021-02-08 15:49:46 +0100

    2

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
-    include 'core/database/connection.php';
+    include 'core/init.php';
     }
 ?>
+
+<html>
+	<head>
+		<center> <title>Twit</title>
+	</head>
+    <body>		
+    <div>
+        <h1>Welcome to Twit!!</h1>
+                
+        <div>
+        <?php include 'includes/login.php';?>
+        </div>
+
+        <div>
+        <?php include 'includes/signup.php';?>
+        </div>
+    </div>
+    </body></center>
+</html>
